c2ad1a3f7dbf8a30ec546e9c12c51a84e8a1b41a
mtlyaumbetov <mtlyaumbetov@ozon.ru>
2024-12-07 17:25:45 +0300

    lection5

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/guap-courses/go
 
 go 1.21
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
---
+++ lection05/3_default_server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloHandler)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+
+	_, _ = w.Write([]byte("hello, World!/n"))
+
+}
